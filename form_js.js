a1e545030cd302a10cea18c0ad530bdb7623ff99
Tesfaye <twondimu@students.kennesaw.edu>
2022-04-22 21:17:52 -0400

    22

---
+++ form_js.js
@@ -0,0 +1,7 @@
+var classification = {
+
+    Freshman :['MATH 116 : Pre-Calculus Math I','CIS 105 : Principles of Computing ', 'Math 213: Calculus I'],
+    Sphomore  :['MAIH 251: Pre-Calculus Math II','CIS 121: Computer Science 1  ', 'ECON241: Principles of Macroeconomics'],
+    Jnior  :['ACCT 408 Internal Auditing','CIS 121: Computer Science 1  ', 'ENGL 222/MODL 222 World Literature'],
+    Senior  :['MAIH 251: Pre-Calculus Math II','CCYBR 291 Research  ', 'IDES 240: Computer-Aided Design for Designers '],
+}
